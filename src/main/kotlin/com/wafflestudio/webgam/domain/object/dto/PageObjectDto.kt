d4a3db9cf68159a6ee76452781c466355369a958
Goniieee <gksrhs97@gmail.com>
2023-03-11 11:55:04 +0900

    feat: 오브젝트 스키마 변경 (#13)

--- src/main/kotlin/com/wafflestudio/webgam/domain/object/dto/PageObjectDto.kt
+++ src/main/kotlin/com/wafflestudio/webgam/domain/object/dto/PageObjectDto.kt
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.PositiveOrZero
+import org.hibernate.validator.constraints.Range
 import org.hibernate.validator.constraints.URL
 import java.time.LocalDateTime
 
@@ -28,12 +29,25 @@ class PageObjectDto {
         val yPosition: Int?,
         @field:[NotNull PositiveOrZero]
         val zIndex: Int?,
+        @field:[NotNull Range(min=0, max=100)]
+        val opacity: Int?,
         /* Optional */
         val textContent: String?,
         @field:Positive
         val fontSize: Int?,
+        @field:Positive
+        val lineHeight: Int?,
+        @field:Positive
+        val letterSpacing: Int?,
+        val backgroundColor: String?,
+        @field:Positive
+        val strokeWidth: Int?,
+        val strokeColor: String?,
         @field:URL
         val imageSource: String?,
+        val isReversed: Boolean?,
+        @field:Range(min=0, max=359)
+        val rotateDegree: Int?,
     )
 
     data class PatchRequest(
@@ -46,11 +60,24 @@ class PageObjectDto {
         val yPosition: Int?,
         @field:PositiveOrZero
         val zIndex: Int?,
+        @field:Range(min=0, max=100)
+        val opacity: Int?,
         val textContent: String?,
         @field:Positive
         val fontSize: Int?,
+        @field:Positive
+        val lineHeight: Int?,
+        @field:Positive
+        val letterSpacing: Int?,
+        val backgroundColor: String?,
+        @field:Positive
+        val strokeWidth: Int?,
+        val strokeColor: String?,
         @field:URL
         val imageSource: String?,
+        val isReversed: Boolean?,
+        @field:Range(min=0, max=359)
+        val rotateDegree: Int?,
     )
 
     data class SimpleResponse(
@@ -66,9 +93,17 @@ class PageObjectDto {
         val xPosition: Int,
         val yPosition: Int,
         val zIndex: Int,
+        val opacity: Int,
         val textContent: String?,
         val fontSize: Int?,
+        val lineHeight: Int?,
+        val letterSpacing: Int?,
+        val backgroundColor: String?,
+        val strokeWidth: Int?,
+        val strokeColor: String?,
         val imageSource: String?,
+        val isReversed: Boolean?,
+        val rotateDegree: Int?,
     ): TimeTraceEntityDto.Response(id, createdAt, createdBy, modifiedAt, modifiedBy) {
         constructor(pageObject: PageObject): this(
             id = pageObject.id,
@@ -83,9 +118,17 @@ class PageObjectDto {
             xPosition = pageObject.xPosition,
             yPosition = pageObject.yPosition,
             zIndex = pageObject.zIndex,
+            opacity = pageObject.opacity,
             textContent = pageObject.textContent,
             fontSize = pageObject.fontSize,
+            lineHeight = pageObject.lineHeight,
+            letterSpacing = pageObject.letterSpacing,
+            backgroundColor = pageObject.backgroundColor,
+            strokeWidth = pageObject.strokeWidth,
+            strokeColor = pageObject.strokeColor,
             imageSource = pageObject.imageSource,
+            isReversed = pageObject.isReversed,
+            rotateDegree = pageObject.rotateDegree,
         )
     }
 
@@ -102,9 +145,17 @@ class PageObjectDto {
         val xPosition: Int,
         val yPosition: Int,
         val zIndex: Int,
+        val opacity: Int,
         val textContent: String?,
         val fontSize: Int?,
+        val lineHeight: Int?,
+        val letterSpacing: Int?,
+        val backgroundColor: String?,
+        val strokeWidth: Int?,
+        val strokeColor: String?,
         val imageSource: String?,
+        val isReversed: Boolean?,
+        val rotateDegree: Int?,
         /* Detailed */
         val pageId: Long,
         val isInteractive: Boolean,
@@ -123,9 +174,17 @@ class PageObjectDto {
             xPosition = pageObject.xPosition,
             yPosition = pageObject.yPosition,
             zIndex = pageObject.zIndex,
+            opacity = pageObject.opacity,
             textContent = pageObject.textContent,
             fontSize = pageObject.fontSize,
+            lineHeight = pageObject.lineHeight,
+            letterSpacing = pageObject.letterSpacing,
+            backgroundColor = pageObject.backgroundColor,
+            strokeWidth = pageObject.strokeWidth,
+            strokeColor = pageObject.strokeColor,
             imageSource = pageObject.imageSource,
+            isReversed = pageObject.isReversed,
+            rotateDegree = pageObject.rotateDegree,
             /* Detailed */
             pageId = pageObject.page.id,
             isInteractive = pageObject.event != null,
