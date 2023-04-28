747148ea393422aba9f0328659b0d9f286029508
huGgW <whjoon0225@naver.com>
2023-04-28 13:43:49 +0900

    Added Pointcut for controllers, and testing Around aspect

---
+++ src/main/kotlin/com/wafflestudio/webgam/global/common/log/LogExecutionTime.kt
@@ -0,0 +1,25 @@
+package com.wafflestudio.webgam.global.common.log
+
+import mu.KotlinLogging
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class LogChecker {
+    private val log = KotlinLogging.logger{}
+
+    @Pointcut("execution(public * com.wafflestudio.webgam..*.controller.*.*(..)) || execution(public * com.wafflestudio.webgam.domain.PingTestController.*(..))")
+    fun controllerCuts() {}
+
+    @Around("controllerCuts()")
+    fun controllerTime(jointPoint: ProceedingJoinPoint): Any? {
+        log.info {"Hello ${jointPoint.signature.toShortString()}"}
+        val result = jointPoint.proceed()
+        log.info {"Bye ${jointPoint.signature.toShortString()}"}
+        return result
+    }
+}
